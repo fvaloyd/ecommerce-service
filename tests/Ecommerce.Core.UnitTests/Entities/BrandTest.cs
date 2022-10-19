93829761c3ab0e1926224df789648cae6dfbb872
francis <francisalcantara001@gmail.com>
2022-10-19 13:11:56 -0400

    Tests for Brand entity added

---
+++ tests/Ecommerce.Core.UnitTests/Entities/BrandTest.cs
@@ -0,0 +1,50 @@
+using Ecommerce.Core.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Ecommerce.Core.UnitTests.Entities;
+
+public class BrandTest
+{
+    [Fact]
+    public void Should_Inherit_BaseEntity()
+    {
+        typeof(Brand).Should().BeAssignableTo<BaseEntity>();
+    }
+
+    [Theory]
+    [InlineData("Dell")]
+    [InlineData("Hp")]
+    [InlineData("Razer")]
+    [InlineData("Predator")]
+    public void SetName_With_Valid_Params_Should_Set_The_Name(string validName)
+    {
+        Brand brandMock = new();
+
+        brandMock.SetName(validName);
+
+        brandMock.Name.Should().Be(validName);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("h")]
+    public void SetName_With_Invalid_Name_Should_Throw_ArgumentException(string invalidName)
+    {
+        Brand brandMock = new();
+
+        Action act = () => brandMock.SetName(invalidName);
+
+        act.Should().Throw<ArgumentException>().WithMessage("The length of the name could not be less than 1");
+    }
+
+    [Fact]
+    public void SetName_With_Null_Name_Should_Throw_ArgumentNullException()
+    {
+        Brand brandMock = new();
+
+        Action act = () => brandMock.SetName(null!);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
