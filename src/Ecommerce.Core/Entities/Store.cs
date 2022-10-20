0495cff7873fd5ba3e5c33bb23940d4803fef484
francis <francisalcantara001@gmail.com>
2022-10-20 19:38:52 -0400

    Constructor added, SetName added & refactor ChangeState

--- src/Ecommerce.Core/Entities/Store.cs
+++ src/Ecommerce.Core/Entities/Store.cs
@@ -4,10 +4,24 @@ public class Store : BaseEntity
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public bool State { get; set; }
+    public bool State { get; set; } = true;
 
-    public void ChangeState(bool newState)
+    public void ChangeState()
     {
-        State = newState;
+        State = !State;
+    }
+
+    public void SetName(string name)
+    {
+        if (name is null) throw new ArgumentNullException();
+        if (name.Count() < 1) throw new ArgumentException("Name length could not be less than 1");
+        Name = name;
+    }
+
+    public Store(){}
+
+    public Store(string name)
+    {
+        Name = name;
     }
 }
