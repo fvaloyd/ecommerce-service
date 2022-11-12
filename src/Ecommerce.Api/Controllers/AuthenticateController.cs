3e98fd3b50c3040f515c1dbfddb1c9cf93857b14
Francis Alcantara <francisalcantara001@gmail.com>
2022-11-12 16:35:01 -0400

    Modify responses

--- src/Ecommerce.Api/Controllers/AuthenticateController.cs
+++ src/Ecommerce.Api/Controllers/AuthenticateController.cs
@@ -15,18 +15,15 @@ public class AuthenticateController : ApiControllerBase
 {
     private readonly IStripeService _stripeService;
     private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManager;
     private readonly ITokenService _tokenService;
     private readonly IEmailSender _emailService;
     public AuthenticateController(
         UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager,
         ITokenService tokenService,
         IStripeService stripeService,
         IEmailSender emailService)
     {
         _userManager = userManager;
-        _roleManager = roleManager;
         _tokenService = tokenService;
         _stripeService = stripeService;
         _emailService = emailService;
@@ -37,7 +34,7 @@ public class AuthenticateController : ApiControllerBase
     {
         ApplicationUser userExist = await _userManager.FindByEmailAsync(model.Email);
 
-        if (userExist != null) return StatusCode(StatusCodes.Status400BadRequest, new Response(Status:HttpStatusCode.InternalServerError, Message: "User already exist"));
+        if (userExist != null) return BadRequest(new Response(HttpStatusCode.BadRequest, "User already exist"));
 
         ApplicationUser user = new()
         {
@@ -52,33 +49,35 @@ public class AuthenticateController : ApiControllerBase
 
         IdentityResult result = await _userManager.CreateAsync(user, model.Password);
 
-        if (!result.Succeeded) return StatusCode(StatusCodes.Status500InternalServerError, new Response(Status:HttpStatusCode.InternalServerError, Message:"User creation failed! Please try again"));
+        if (!result.Succeeded) return BadRequest(new Response(HttpStatusCode.InternalServerError, "User creation failed! Please try again"));
 
         await _userManager.AddToRoleAsync(user, UserRoles.User);
 
-        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user); 
-        var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authenticate", new { token, email = user.Email}, Request.Scheme);
+        await SendMailToConfirmEmail(user);
 
-        var mailRequest = await CreateMailRequest(user, confirmationLink!);
-        await _emailService.SendAsync(mailRequest);
-
-        return Ok(new Response(Status: HttpStatusCode.OK, Message: "Check you mail message and confirm your email"));
+        return Ok(new Response(HttpStatusCode.OK, "Check you mail message and confirm your email"));
     }
 
     [HttpGet("confirm-email", Name = "ConfirmEmail")]
     public async Task<ActionResult> ConfirmEmail(string token, string email)
     {
         var user = await _userManager.FindByEmailAsync(email);
-        if (user is null) return NotFound("User not found");
+
+        if (user is null) return NotFound(new Response(HttpStatusCode.NotFound, "User not found"));
+
         var result = await _userManager.ConfirmEmailAsync(user, token);
-        if (!result.Succeeded) return BadRequest("Could not confirm the email");
-        return Ok("Email confirm successfully");
+
+        if (!result.Succeeded) return BadRequest(new Response(HttpStatusCode.BadRequest, "Could not confirm the email"));
+
+        return Ok(new Response(HttpStatusCode.OK, "Email confirm successfully"));
     }
 
     private async Task<MailRequest> CreateMailRequest(ApplicationUser user, string confirmationLink)
     {
         var emailConfirmationMailModel = new EmailConfirmationMailModel(User: user, ConfirmationLink: confirmationLink);
+
         string template = await _emailService.GetTemplate(((int)MailTemplates.EmailConfirmation));
+
         string compiledTemplate = await _emailService.GetCompiledTemplateAsync(template, emailConfirmationMailModel);
 
         return new MailRequest(
@@ -95,7 +94,7 @@ public class AuthenticateController : ApiControllerBase
         ApplicationUser userExist = await _userManager.FindByEmailAsync(model.Email);
 
         if (userExist != null)
-            return StatusCode(StatusCodes.Status500InternalServerError, new Response( Status: HttpStatusCode.InternalServerError, Message: "User already exist"));
+            return BadRequest(new Response(HttpStatusCode.BadRequest, "User already exist"));
 
         ApplicationUser user = new()
         {
@@ -105,13 +104,18 @@ public class AuthenticateController : ApiControllerBase
         };
 
         var customer = await _stripeService.CreateCustomerToken(user);
+
         user.CustomerId = customer.Id;
 
         IdentityResult result = await _userManager.CreateAsync(user, model.Password);
 
+        if (!result.Succeeded) return BadRequest(new Response(HttpStatusCode.BadRequest, "Error occurred creating the user"));
+
         await _userManager.AddToRoleAsync(user, UserRoles.Admin);
 
-        return Ok(new Response(Status: HttpStatusCode.OK, Message: "User created successfully"));
+        await SendMailToConfirmEmail(user);
+
+        return Ok(new Response(HttpStatusCode.OK, "User created successfully"));
     }
 
     [HttpPost("login")]
@@ -126,15 +130,17 @@ public class AuthenticateController : ApiControllerBase
         if (!isEmailConfirmed)
         {
             await SendMailToConfirmEmail(user);
-            return BadRequest("You need to confirm your email. Check your mail to confirm");
+            return BadRequest(new Response(HttpStatusCode.BadRequest, "You need to confirm your email. Check your mail to confirm"));
         }
 
-        if (!await _userManager.CheckPasswordAsync(user, model.Password)) return BadRequest("Incorrect password");
+        if (!await _userManager.CheckPasswordAsync(user, model.Password)) return BadRequest(new Response(HttpStatusCode.BadRequest, "Incorrect password"));
 
         string accessToken = await _tokenService.CreateToken(model);
+
         string RefreshToken = _tokenService.CreateRefreshToken();
 
         user.RefreshToken = RefreshToken;
+
         user.RefreshTokenExpireTime = DateTime.Now.AddHours(3);
 
         await _userManager.UpdateAsync(user);
@@ -143,10 +149,12 @@ public class AuthenticateController : ApiControllerBase
     }
     private async Task SendMailToConfirmEmail(ApplicationUser user)
     {
-        var emailConfirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user); 
-        var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authenticate", new { emailConfirmationToken, email = user.Email}, Request.Scheme);
+        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user); 
+
+        var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authenticate", new { token = token, email = user.Email}, Request.Scheme);
 
         var mailRequest = await CreateMailRequest(user, confirmationLink!);
+
         await _emailService.SendAsync(mailRequest);
     }
 }
