58021932edab5be3c5ccf240c16524dbe94a1131
Francis Alcantara <francisalcantara001@gmail.com>
2022-12-27 01:18:00 -0400

    Use dbcontext instead of generic repository

--- src/Ecommerce.Application/Stores/IStoreService.cs
+++ src/Ecommerce.Application/Stores/IStoreService.cs
@@ -5,5 +5,5 @@ public interface IStoreService
     Task<bool> AddProductAsync(int productId, int storeId);
     Task<bool> IncreaseProductAsync(int productId, int storeId);
     Task<bool> DecreaseProductAsync(int productId, int storeId);
-    void DeleteProductStoreRelation(int storeId);
+    Task<bool> DeleteProductStoreRelation(int storeId);
 }
